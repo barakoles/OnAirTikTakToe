85a6e2d03f21d2a89110fd5e2f85fab58b2151d8
Matheus <matheus_paranhos@outlook.com.br>
2022-08-17 15:12:30 +0100

    feat: :fire: Setup Redux and redux store

--- src/navigation/registerScreens.tsx
+++ src/navigation/registerScreens.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Navigation } from 'react-native-navigation';
+import { Provider } from 'react-redux';
+import { store } from '@redux/store';
+
 import { Screens } from './Screens';
+
 import App from '../../App';
 
 function WrappedComponent(Component: any) {
   return function inject(props: any) {
-    const EnhancedComponent = () => <Component {...props} />;
+    const EnhancedComponent = () => (
+      <Provider store={store}>
+        <Component {...props} />
+      </Provider>
+    );
     return <EnhancedComponent />;
   };
 }
